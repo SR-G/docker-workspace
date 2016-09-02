5ed20adcc0bf782060c2ff9879fa11bd8fced964
SR-G <serge.simon@gmail.com>
2016-09-02 17:27:15 +0200

    Switcher server to 6.0.2

--- tensin-app-seafile/download-seafile.sh
+++ tensin-app-seafile/download-seafile.sh
@@ -2,7 +2,7 @@
 
 # Documentation here : http://manual.seafile.com/deploy/common_problems_for_setting_up_server.html
 
-SEAFILE_VERSION=4.3.2
+SEAFILE_VERSION=6.0.2
 SEAFILE_ARCHIVE_URL="https://bintray.com/artifact/download/seafile-org/seafile/seafile-server_${SEAFILE_VERSION}_x86-64.tar.gz"
 
 cd /opt/seafile
